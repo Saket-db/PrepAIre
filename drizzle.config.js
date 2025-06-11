6bf08dac789439e2fc36cd4415be8f6e1769c522
Saket-db <dbsaket04@gmail.com>
2025-06-12 03:13:38 +0530

    url  updated

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   schema: "./utils/schema.js",
 //   out: "./drizzle",
 dbCredentials: {
-    url: 'ostgresql://neondb_owner:npg_c7S0wqgTMHEG@ep-small-bonus-a58c5fjk-pooler.us-east-2.aws.neon.tech/PrepAIre?sslmode=require',
+    url: 'postgresql://neondb_owner:npg_c7S0wqgTMHEG@ep-small-bonus-a58c5fjk-pooler.us-east-2.aws.neon.tech/PrepAIre?sslmode=require',
 }
 });
